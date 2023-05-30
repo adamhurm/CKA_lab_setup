8f0865a8f2ad65010c72087b690d7abf42bb891b
Adam Hurm <adamhurm@users.noreply.github.com>
2023-05-29 20:59:53 -0400

    working modular update haproxy

--- kubernetes-setup/config_files/update-haproxy-cfg.py
+++ kubernetes-setup/config_files/update-haproxy-cfg.py
@@ -1,54 +1,64 @@
 import argparse, re
 
 def build_config(args):
-    listen_cfg = """
+    default_frontend_cfg = """
+frontend proxynode
+    bind *:80
+    bind *:6443
+    stats uri /proxystats
+"""
+    default_backend_cfg = """
+backend k8sServers
+    balance roundrobin
+"""
+    default_listen_cfg = """
 listen stats
     bind :9999
     mode http
     stats enable
     stats hide-version
     stats uri /stats
-    """
-    frontend_cfg = """
-frontend proxynode
-    bind *:80
-    bind *:6443
-    stats uri /proxystats
-"""
-    backend_cfg = """
-backend k8sServers
-    balance roundrobin
 """
 
-    with open('haproxy.cfg') as f:
+    with open('haproxy.cfg', 'r+') as f:
         cfg = f.read()
 
-        read_defaults_cfg = re.search(r'defaults(\n(\t|\ )+.*)+\n', cfg).group(0)
-        defaults_cfg = re.sub(r'mode\shttp', 'mode\ttcp', read_defaults_cfg)
-        defaults_cfg = re.sub(r'option\shttplog', 'option\ttcplog', defaults_cfg)
-        cfg.replace(read_defaults_cfg, defaults_cfg)
+        global_cfg = re.search(r'global(\n(\t|\ )+.*)+\n', cfg).group(0)
 
-        read_backend_cfg = re.search(r'backend k8sServers(\n(\t|\ )+.*)+', cfg).group(0)
-        if read_backend_cfg:
-            backend_cfg = read_backend_cfg
+        defaults_cfg = re.search(r'defaults(\n(\t|\ )+.*)+\n', cfg).group(0)
+        defaults_cfg = re.sub(r'mode\shttp', 'mode\ttcp', defaults_cfg)
+        defaults_cfg = re.sub(r'option\shttplog', 'option\ttcplog', defaults_cfg)
+
+        frontend_cfg = re.search(r'frontend proxynode(\n(\t|\ )+.*)+\n', cfg)
+        if frontend_cfg:
+            frontend_cfg = frontend_cfg.group(0)
+        else:
+            frontend_cfg = default_frontend_cfg
+
+        backend_cfg = re.search(r'backend k8sServers(\n(\t|\ )+.*)+\n', cfg)
+        if backend_cfg:
+            backend_cfg = backend_cfg.group(0)
+        else:
+            backend_cfg = default_backend_cfg
+
+        listen_cfg = re.search(r'listen stats(\n(\t|\ )+.*)+\n', cfg)
+        if listen_cfg:
+            listen_cfg = listen_cfg.group(0)
+        else:
+            listen_cfg = default_listen_cfg
 
         add_node = lambda c, n, i: c+f'    server {n}  {i}:6443 check\n'
         for node_pair in args.nodes:
             name, ip = node_pair.split('=')
             backend_cfg = add_node(backend_cfg, name, ip)
 
-        if args.u:
-            print(cfg)
-        else:
-            print(cfg + frontend_cfg + backend_cfg + listen_cfg)
+        f.write(global_cfg+'\n'+defaults_cfg+'\n'+frontend_cfg+'\n'+backend_cfg+'\n'+listen_cfg+'\n')
 
 def main():
     parser = argparse.ArgumentParser(prog='UpdateHaproxyCfg',
                                     description='Update haproxy.cfg for HA k8s cluster')
     parser.add_argument('nodes', nargs='+', help='provide list: node1=ip1 name2=ip2 ...',
                         metavar="KEY=VALUE")
-    parser.add_argument('-u', help='update existing values instead of starting from scratch',
-                         action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
     build_config(args)
 
